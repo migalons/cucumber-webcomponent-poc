ff7be8f40feb1542f2fb54f07dc4a8636bb4a9ee
Miguel A. Alonso <miguel-angel.alonso@soprasteria.com>
2018-05-14 13:15:01 +0200

    Allow cucumber.js to receive several profile request

--- cucumber.js
+++ cucumber.js
@@ -4,25 +4,42 @@ const path = require('path');
 const minimist = require('minimist');
 const fs = require('fs');
 
-let profiles = {
-    "base": "--world-parameters={\"config\":\"config/base.js\"}"
+let baseProfiles = {
+    "base": "--world-parameters={\"config\":\"config/base.js\"}",
+    "no-colours": " --format-options '{\"colorsEnabled\": false}'"
 };
 
 module.exports = function () {
-    let requestedProfile = minimist(process.args.slice(2)).p || minimist(process.args.slice(2)).profile;
-    let customizedProfile = {};
+    let requestedProfiles = minimist(process.argv.slice(2)).p || minimist(process.argv.slice(2)).profile;
+    let customizedProfile = baseProfiles;
 
-    if(profiles[requestedProfile]) {
-        return profiles;
-    } else {
-        let configFilePath = path.resolve(requestedProfile);
-        try {
-            fs.accessSync(configFilePath, fs.constants.R_OK);
-            customizedProfile[requestedProfile] = `--world-parameters '{"config": "${requestedProfile}"}'`
-        } catch (e) {
-            console.error(e)
+    if(typeof requestedProfiles == "string") {
+        requestedProfiles = new Array(requestedProfiles);
+    };
+
+    for(let i in requestedProfiles) {
+        let requestedProfile = requestedProfiles[i]
+        if(!baseProfiles[requestedProfile]) {
+            let configFilePath = path.resolve(requestedProfile);
+            try {
+                fs.accessSync(configFilePath, fs.constants.R_OK);
+                customizedProfile[requestedProfile] =  `--world-parameters '{"config": "${requestedProfile}"}'`
+            } catch (e) {
+                console.error(e)
+            }
         }
     }
+    // if(baseProfiles[requestedProfile]) {
+    //     return baseProfiles;
+    // } else {
+    //     let configFilePath = path.resolve(requestedProfile);
+    //     try {
+    //         fs.accessSync(configFilePath, fs.constants.R_OK);
+    //         customizedProfile[requestedProfile] =  `--world-parameters '{"config": "${requestedProfile}"}'`
+    //     } catch (e) {
+    //         console.error(e)
+    //     }
+    // }
 
     return customizedProfile
-}
+}();
